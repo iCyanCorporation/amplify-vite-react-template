c7f1e255b4a1ff95e0420397442eae2c2b65fbe6
hello112334 <hello112334@gmail.com>
2025-07-10 11:14:33 +0900

    Add Redux for state management and implement counter functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 import { useTranslation } from "react-i18next";
+import { useAppDispatch, useAppSelector } from "./store/hooks";
+import { decrement, increment } from "./store/slices/counterSlice";
 
 const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
   const { t, i18n } = useTranslation();
+
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -32,13 +37,27 @@ function App() {
       <h1>{t("welcome")}</h1>
       <p>{t("description.part1")}</p>
       <p>{t("description.part2")}</p>
-      <h2>My todos</h2>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
+      {/* counter */}
+      <div className="w-full">
+        <h2>Counter</h2>
+        <div className="flex items-center justify-center gap-2">
+          <button onClick={() => dispatch(decrement())}>-</button>
+          <span>count is {count}</span>
+          <button onClick={() => dispatch(increment())}>+</button>
+        </div>
+      </div>
+      {/* todo list */}
+      <div>
+        <h2>My todos</h2>
+        <button className="w-full" onClick={createTodo}>
+          + new
+        </button>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>{todo.content}</li>
+          ))}
+        </ul>
+      </div>
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
